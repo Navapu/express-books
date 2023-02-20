cbc496a4213349901baf155a49da55067f1d73f3
Navapu <drojannp2@gmail.com>
2023-02-20 15:04:20 +0100

    Creo el json para guardar los datos del formulario

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const fs = require ('fs')
 
 const libros = []
 
 router.get('/', (req , res) => {
-    res.render('index.hbs')
+    res.render('index.hbs', {
+        libros
+    })
 })
 
 router.get('/formulario', (req, res) =>{
     res.render('formulario')
 })
 router.post('/formulario', (req, res) => {
-    book.push(req.body)
+    const {title, imagen, descripcion} = req.body
+    let newBook = {
+        title,
+        imagen,
+        descripcion
+    }
+    libros.push(newBook)
+    const json_libros = JSON.stringify(libros)
+    fs.writeFileSync('libros.json', json_libros, 'utf-8')
+
     res.send('Recibido');
 })
 module.exports = router;
