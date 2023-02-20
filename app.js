9f3db111d54f006b8dd260d396fffd50048cbd97
Navapu <drojannp2@gmail.com>
2023-02-20 12:22:33 +0100

    Empiezo el proyecto con HandleBars

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const { engine } = require('express-handlebars')
+const path = require('path')
+const indexRouter = require('./routes/index');
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', engine({
+  defaultLayout: 'main',
+  layoutsDir:  path.join(app.get('views'), 'layouts'),
+  partialsDir:  path.join(app.get('views'), 'partials'),
+  extname: '.hbs',
+  helpers: require('./lib/handlebars')
+}))
+
+app.set('view engine', '.hbs');
+
+app.use('/', indexRouter)
+
+app.listen(3000)
+console.log('Escuchando en el puerto 3000')
