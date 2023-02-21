fb8c299259a3cae459b7854fcf06624f00d626f5
Navapu <drojannp2@gmail.com>
2023-02-21 01:33:37 +0100

    Añadimos la opcion de borrar los libros. APP FINALIZADA

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const fs = require ('fs')
-
-const libros = []
+const {v4: uuidv4} = require('uuid')
+const json_libros = fs.readFileSync('libros.json', 'utf-8')
+let libros = JSON.parse(json_libros)
 
 router.get('/', (req , res) => {
     res.render('index.hbs', {
@@ -16,6 +17,7 @@ router.get('/formulario', (req, res) =>{
 router.post('/formulario', (req, res) => {
     const {title, imagen, descripcion} = req.body
     let newBook = {
+        id: uuidv4(),
         title,
         imagen,
         descripcion
@@ -24,6 +26,13 @@ router.post('/formulario', (req, res) => {
     const json_libros = JSON.stringify(libros)
     fs.writeFileSync('libros.json', json_libros, 'utf-8')
 
-    res.send('Recibido');
+    res.redirect('/')
+})
+
+router.get('/delete/:id', (req, res)=>{
+    libros = libros.filter(libro => libro.id != req.params.id)
+    const json_libros = JSON.stringify(libros)
+    fs.writeFileSync('libros.json', json_libros, 'utf-8')
+    res.redirect('/')
 })
 module.exports = router;
